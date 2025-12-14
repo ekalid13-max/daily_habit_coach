6aab467b6f08671f0042c469e4f72c19f0596112
ekalid13-max <ekalid13@gmail.com>
2025-12-14 02:52:59 +0200

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String id;
+  final String email;
+  final String name;
+  final bool premium;
+  final DateTime createdAt;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.premium,
+    required this.createdAt,
+  });
+
+  factory UserModel.fromMap(String id, Map<String, dynamic> data) =>
+      UserModel(
+        id: id,
+        email: data['email'],
+        name: data['name'],
+        premium: data['premium'] ?? false,
+        createdAt: (data['createdAt'] as Timestamp).toDate(),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'email': email,
+        'name': name,
+        'premium': premium,
+        'createdAt': Timestamp.fromDate(createdAt),
+      };
+}
 
