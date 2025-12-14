cb31679335b19d4d4adc3923e5bf1d01980d4e6f
ekalid13-max <ekalid13@gmail.com>
2025-12-14 02:39:27 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/theme.dart';
+import 'screens/auth/auth_screen.dart';
+import 'screens/home/home_screen.dart';
+import 'services/auth_service.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AuthService(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily Habit Coach',
+      theme: appTheme,
+      home: Consumer<AuthService>(
+        builder: (_, auth, __) =>
+            auth.currentUser == null ? const AuthScreen() : const HomeScreen(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
